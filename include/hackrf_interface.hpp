3ef947623d909b37ff79c8a4b7c50d2e691e65de
Ryan Campbell <rdcampbell@tamu.edu>
2017-10-09 16:28:49 -0500

    Clang-format

--- include/hackrf_interface.hpp
+++ include/hackrf_interface.hpp
@@ -4,50 +4,53 @@
 #ifndef HACKRF_INTERFACE_HPP
 #define HACKRF_INTERFACE_HPP
 
+#include <boost/log/trivial.hpp>
 #include <exception>
 #include <iostream>
-#include <libhackrf/hackrf.h>
 #include <memory>
 #include <sstream>
 #include <string>
+
+#include <libhackrf/hackrf.h>
 
 namespace HackRF {
 
 enum HackRFStatus { Open, Closed, Error };
 
 class HackRFException : public std::exception {
-  using std::exception::what;
+    using std::exception::what;
 
-public:
-  HackRFException(int result);
-  ~HackRFException() throw(){};
+  public:
+    HackRFException() = delete;
+    explicit HackRFException(int result);
+    ~HackRFException() throw(){};
 
-  const char *what();
+    const char *what();
 
-private:
-  std::string msg;
+  private:
+    std::string msg;
 };
 
 class HackRFInterface {
-public:
-  HackRFInterface();
-  ~HackRFInterface();
+  public:
+    HackRFInterface();
+    ~HackRFInterface();
 
-  void open();
-  void close();
+    void open();
+    void close();
 
-  std::string info();
+    void info();
 
-private:
-  HackRFStatus status;
-  char *serial_no;
-  char version[256];
-  uint8_t board_id;
-  read_partid_serialno_t read_partid_serialno;
-  hackrf_device_list_t *device_list;
-  hackrf_device *device;
+  private:
+    HackRFStatus status;
+    char *serial_no;
+    char version[256];
+    uint8_t board_id;
+    read_partid_serialno_t read_partid_serialno;
+    hackrf_device_list_t *device_list;
+    hackrf_device *device;
 
-  void check_result(int result);
+    void check_result(int result);
 };
 }
 #endif
--- src/hackrf_interface.cpp
+++ src/hackrf_interface.cpp
@@ -7,62 +7,67 @@
 namespace HackRF {
 
 HackRFException::HackRFException(int result) {
-  auto err_num = static_cast<hackrf_error>(result);
-  msg = hackrf_error_name(err_num);
+    auto err_num = static_cast<hackrf_error>(result);
+    msg = hackrf_error_name(err_num);
 }
 
+const char *HackRFException::what() { return msg.c_str(); }
+
 HackRFInterface::HackRFInterface() {
-  status = HackRFStatus::Closed;
-  serial_no = 0;
-  device_list = nullptr;
-  device = nullptr;
+    status = HackRFStatus::Closed;
+    serial_no = 0;
+    device_list = nullptr;
+    device = nullptr;
 }
 
 HackRFInterface::~HackRFInterface() {}
 
 void HackRFInterface::open() {
-  int result = hackrf_init();
-  check_result(result);
-  device_list = hackrf_device_list();
-  if (device_list->devicecount < 1) {
-    throw std::runtime_error("No HackRFs Found");
-  }
-  if (device_list->devicecount != 1) {
-      std::cerr
-        << "More than one HackRF found. Choosing first available";
-  }
-  serial_no = device_list->serial_numbers[0];
+    int result = hackrf_init();
+    check_result(result);
+    device_list = hackrf_device_list();
+    if (device_list->devicecount < 1) {
+        throw std::runtime_error("No HackRFs Found");
+    }
+    if (device_list->devicecount != 1) {
+        BOOST_LOG_TRIVIAL(warning)
+            << "More than one HackRF found. Choosing first available";
+    }
+    serial_no = device_list->serial_numbers[0];
 
-  result = hackrf_device_list_open(device_list, 0, &device);
-  check_result(result);
+    result = hackrf_device_list_open(device_list, 0, &device);
+    check_result(result);
 
-  result = hackrf_board_id_read(device, &board_id);
-  check_result(result);
+    result = hackrf_board_id_read(device, &board_id);
+    check_result(result);
 
-  result = hackrf_version_string_read(device, version, 255);
-  check_result(result);
+    result = hackrf_version_string_read(device, version, 255);
+    check_result(result);
 
-  result = hackrf_board_partid_serialno_read(device, &read_partid_serialno);
-  check_result(result);
+    result = hackrf_board_partid_serialno_read(device, &read_partid_serialno);
+    check_result(result);
 }
 
 void HackRFInterface::close() {}
 
-std::string HackRFInterface::info() {
-  std::stringstream ss;
-
-  ss << std::endl << "HackRF Information: " << std::endl;
-  ss << "\tSerial number: " << std::hex << (int *)serial_no << std::endl;
-  ss << "\tBoard ID number: " << (int)board_id << std::endl;
-  ss << "\tPart ID number: " << std::hex << read_partid_serialno.part_id << "\t"
-     << std::hex << read_partid_serialno.serial_no << std::endl;
-
-  return ss.str();
+void HackRFInterface::info() {
+    if (status != HackRFStatus::Open) {
+        BOOST_LOG_TRIVIAL(warning)
+            << "Connection to HackRF is not open. Attempting to open";
+        this->open();
+    }
+    BOOST_LOG_TRIVIAL(info) << "HackRF Information: ";
+    BOOST_LOG_TRIVIAL(info)
+        << "\t  Serial   number: " << std::hex << (int *)serial_no;
+    BOOST_LOG_TRIVIAL(info) << "\t  Board ID number: " << (int)board_id;
+    BOOST_LOG_TRIVIAL(info)
+        << "\t  Part  ID number: " << std::hex << read_partid_serialno.part_id
+        << "  " << std::hex << read_partid_serialno.serial_no;
 }
 
 void HackRFInterface::check_result(int result) {
-  if (result != HACKRF_SUCCESS) {
-    throw HackRF::HackRFException(result);
-  }
+    if (result != HACKRF_SUCCESS) {
+        throw HackRF::HackRFException(result);
+    }
 }
 } // namespace HackRF
