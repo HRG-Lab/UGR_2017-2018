55f7e2e31e5bad4ccc0da72ac986417009b882e3
jfreking <jacobfreking@gmail.com>
2017-11-13 14:13:21 -0600

    Add files via upload

--- jfreking/KinectComputerVision/kinect_recognition_dlib.py
+++ jfreking/KinectComputerVision/kinect_recognition_dlib.py
@@ -1,27 +1,45 @@
 #import the necessary modules
 import glob
 import dlib
-from skimage import io
-from skimage.draw import polygon_perimeter
-import sys
-import os
-#from time import sleep
-import timeit
 import freenect
 import cv2
+import timeit
 import numpy as np
-import operator
 import calibkinect
 import serial
 import time
- 
-time.sleep(1)
-WIDTH  = 0.252 # m
-HEIGHT = 0.252 # m
-#detector = dlib.simple_object_detector("/home/jfreking/dlib/detector.svm")
+import struct
+
+import calculate_phase_CV_Targetting as phase
+
+class packet_t:
+    def __init__(self,phases):
+        self.phases = phases #calculated phases for each element
+        
+    def ba(self):
+        for i in range(len(phases)):
+            ba_curr = bytearray(struct.pack("f",self.phases[i]))
+            if i == 0:
+                ba = ba_curr
+            else:
+                ba = np.concatenate((ba,ba_curr))
+            
+        #bar = bytearray(struct.pack("f",self.r))
+        #bat = bytearray(struct.pack("f",self.t))
+        #bap = bytearray(struct.pack("f",self.f))
+        return ba #np.concatenate((bar,np.concatenate((bat,bap))))
 
 BAUD = 115200
-#arduino = serial.Serial('/dev/ttyACM0', BAUD, timeout=0.1)
+
+arduino = serial.Serial('/dev/ttyACM0', BAUD, timeout=0.1)
+
+time.sleep(1) #give the connection a sec to settle
+
+#WIDTH  = 0.252 # m
+#HEIGHT = 0.252 # m
+
+BAUD = 115200
+arduino = serial.Serial('/dev/ttyACM0', BAUD, timeout=0.1)
 
 #function to get RGB image from kinect
 def get_video():
@@ -34,14 +52,66 @@ def get_depth():
     depth,_ = freenect.sync_get_depth()
     array = depth.astype(np.uint8) #must be an uint8 to display, but must be raw to find position
     return array, depth
-    
+
+#setup the detector and start a timer for kicks    
 def initialSetup():
     #os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
     detector = dlib.simple_object_detector("/home/jfreking/dlib/detector.svm")
     start_time = timeit.default_timer()
     return detector
     
- 
+#send packet of phases to arduino
+def sendSerial(phases):
+    val = packet_t(phases)
+    ba = val.ba()
+    arduino.write(ba)
+    arduino.flush() 
+
+
+def calcPosition(dets):
+    for k, d in enumerate(dets):
+        #print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+        #    k, d.left(), d.top(), d.right(), d.bottom()))
+            
+        u,v = np.mgrid[d.left():d.right(), d.top():d.bottom()]
+        #print("Height/y-pixel: {}".format(HEIGHT/(d.bottom()-d.top())))
+        #print("Width/x-pixel:  {}".format(WIDTH /(d.right()-d.left())))
+        #print(" ")
+                        
+    xyz, uv = calibkinect.depth2xyzuv(depth1[v,u], u, v)
+                                    
+    if len(xyz) != 0:
+                
+        #print(depth_dets[k-1].bottom()) #+= 20
+        #depth_dets[k-1].set_bottom(depth_dets[k-1].bottom()+20)
+        #print(depth_dets[k-1].bottom())
+                
+        #x_ind = np.argmin(abs(xyz[:,0]))
+        #y_ind = len(xyz[:,1])/2 #np.argmin(abs(xyz[:,1]))            
+        z_ind = len(xyz[:,2])/2#np.argmin(abs(xyz[:,2]))
+                
+        x = np.median(xyz[:,0]) #xyz[x_ind,0] #+ 0.03 
+        y = np.median(xyz[:,1]) #xyz[y_ind,1] #+ 0.04
+        z = np.median(xyz[:,2])#*(1-.270718) #xyz[z_ind,2] #- 0.04
+                
+        x = x*100 + 3.3 + z*9.01e-1
+        y = y*100 + 15 - z*4.87
+        z = z*100 - 44.6 - z*21
+                
+        x = x/100
+        y = y/100
+        z = z/100
+        
+        rect = dlib.rectangle( left=np.median(uv[:,0])-5, right=np.median(uv[:,0])+5, 
+            top=np.median(uv[:,1])-5, bottom=np.median(uv[:,1])+5 )
+                
+        print("x [m]: {}".format(x))
+        print("y [m]: {}".format(y))            
+        print("z [m]: {}".format(z))
+        print(" ")  
+
+    return x, y, z, rect
+
 if __name__ == "__main__":
     
     #mdev = freenect.open_device(freenect.init(), 0)
@@ -49,13 +119,14 @@ if __name__ == "__main__":
     #freenect.runloop(dev=mdev)
     
     detector = initialSetup()
-    
+    i = 0
     win_rgb = dlib.image_window()
     win_depth = dlib.image_window()
     win_det = dlib.image_window()
     win_det.set_image(detector)
     
     while True:
+        i = i+1
         #sleep(0.01)
         #get a frame from RGB camera
         frame = get_video()
@@ -73,63 +144,34 @@ if __name__ == "__main__":
         #numpy_frame = cv2.normalize(numpy_frame.astype('float'), None, -0.5, 0.5, cv2.NORM_MINMAX)
         #numpy_final = np.expand_dims(numpy_frame, axis=0)
         
-        dets = detector(frame)
+        if i == 5:
+            i = 0
+            dets = detector(frame)
                         
-        #print("Number of objects detected: {}".format(len(dets)))
-        for k, d in enumerate(dets):
-            #print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
-            #    k, d.left(), d.top(), d.right(), d.bottom()))
-            
-            u,v = np.mgrid[d.left():d.right(), d.top():d.bottom()]
-            print("Height/y-pixel: {}".format(HEIGHT/(d.bottom()-d.top())))
-            print("Width/x-pixel:  {}".format(WIDTH /(d.right()-d.left())))
+            #print("Number of objects detected: {}".format(len(dets)))
+        
+            x,y,z,rect = calcPositions(dets)
+        
+            phases = phase.calcPhase(x,y,z)
+        
+            print(phases)
             print(" ")
-                        
-            xyz, uv = calibkinect.depth2xyzuv(depth1[v,u], u, v)
-                                    
-            if len(xyz) != 0:
-                
-                #print(depth_dets[k-1].bottom()) #+= 20
-                #depth_dets[k-1].set_bottom(depth_dets[k-1].bottom()+20)
-                #print(depth_dets[k-1].bottom())
-                
-                #x_ind = np.argmin(abs(xyz[:,0]))
-                #y_ind = len(xyz[:,1])/2 #np.argmin(abs(xyz[:,1]))            
-                z_ind = len(xyz[:,2])/2#np.argmin(abs(xyz[:,2]))
-                
-                x = np.median(xyz[:,0]) #xyz[x_ind,0] #+ 0.03 
-                y = np.average(xyz[:,1]) #xyz[y_ind,1] #+ 0.04
-                z = np.median(xyz[:,2]) #xyz[z_ind,2] #- 0.04
-                
-                print("x [m]: {}".format(x))
-                print("y [m]: {}".format(y))            
-                print("z [m]: {}".format(z))
-                print(" ")
-                print("uv: {}".format(uv))
-                print(" ")
-                p = np.sqrt(x**2 + y**2 + z**2)
-                theta = np.arccos(y/p)
-                phi = np.arctan(-z/x)
-                
-                #print("p [m]: {}".format(p))
-                #print("theta [rad]: {}".format(theta))
-                #print("phi [rad]: {}".format(phi))
-                #print(" ")
-                
-                #arduino.write(z.astype('float'))
-                #data = arduino.readline()
-                
-                #if data:
-                #    print data.rstrip('\n')
-            
+        
+            sendSerial(phases)
         
         win_rgb.clear_overlay()
         win_rgb.set_image(frame)
         win_rgb.add_overlay(dets)
         
+        if (rect.is_empty() == False):
+            win_rgb.add_overlay(rect)
+        
         win_depth.clear_overlay()
         win_depth.set_image(depth)
         win_depth.add_overlay(dets)
+        
+        if (rect.is_empty() == False):
+            win_depth.add_overlay(rect)
         
         #classify(numpy_final)
         #predictions = sess.run(softmax_tensor, {'Mul:0': numpy_final})
